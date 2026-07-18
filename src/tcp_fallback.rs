3f2ff9ae0d380befb5b63873930cf4aa8467c5e3
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-18 15:46:20 -0300

    Update tcp_fallback.rs

--- src/tcp_fallback.rs
+++ src/tcp_fallback.rs
@@ -3,21 +3,8 @@ use tokio::net::TcpStream;
 use anyhow::Result;
 use log::info;
 
-pub async fn handle_tcp(mut socket: TcpStream) -> Result<()> {
-    info!("📦 TCP echo");
-    
-    let mut buf = [0u8; 1024];
-    loop {
-        match socket.read(&mut buf).await {
-            Ok(0) => break,
-            Ok(n) => {
-                let msg = String::from_utf8_lossy(&buf[..n]);
-                let response = format!("TCP: {}", msg);
-                socket.write_all(response.as_bytes()).await?;
-            }
-            Err(e) => anyhow::bail!("TCP error: {}", e),
-        }
-    }
-    
+pub async fn handle(mut socket: TcpStream) -> Result<()> {
+    info!("📦 TCP");
+    socket.write_all(b"TCP OK\n").await?;
     Ok(())
 }
