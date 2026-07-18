facef2ce2c8e1cea86a1cb4aaf37496d6ea53adb
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-18 17:38:24 -0300

    Update tls.rs

--- src/tls.rs
+++ src/tls.rs
@@ -3,8 +3,8 @@ use anyhow::Result;
 use log::info;
 use tokio::io::AsyncWriteExt;
 
-pub async fn handle(mut socket: TcpStream) -> Result<()> {
-    info!("🔒 TLS");
-    socket.write_all(b"TLS OK\n").await?;
+pub async fn handle_tls(mut socket: TcpStream) -> Result<()> {
+    info!("🔒 TLS/SECURITY");
+    socket.write_all(b"TLS/SECURITY OK\n").await?;
     Ok(())
 }
