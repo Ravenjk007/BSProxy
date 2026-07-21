da27be2ca1a193f28e399045c441432db2777ddd
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-21 14:16:54 -0300

    Update ssh.rs

--- src/ssh.rs
+++ src/ssh.rs
@@ -1,52 +1,16 @@
 use tokio::net::TcpStream;
-use tokio::io::{AsyncRead, AsyncWrite};
-use rustls::ClientConfig;
-use std::sync::Arc;
-use tokio_rustls::TlsConnector;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, copy_bidirectional};
+use std::error::Error;
+use log::info;
 
-pub struct SSHHandler;
-
-impl SSHHandler {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub async fn handle<T: AsyncRead + AsyncWrite + Unpin + Send>(
-        &self,
-        mut stream: T,
-    ) -> Result<(), anyhow::Error> {
-        log::info!("Handling SSH connection with SSL tunneling");
-        
-        // Configuração SSL/TLS
-        let config = self.create_tls_config()?;
-        let connector = TlsConnector::from(Arc::new(config));
-        
-        // Conecta ao servidor SSH via TLS
-        let tcp = TcpStream::connect("seu_servidor_ssh:22").await?;
-        let tls_stream = connector.connect("seu_dominio", tcp).await?;
-        
-        // Encaminha dados entre o cliente e o servidor SSH via TLS
-        self.proxy_data(stream, tls_stream).await?;
-        
-        Ok(())
-    }
-
-    fn create_tls_config(&self) -> Result<rustls::ClientConfig, anyhow::Error> {
-        let mut config = rustls::ClientConfig::builder()
-            .with_safe_defaults()
-            .with_root_certificates(rustls::RootCertStore::empty())
-            .with_no_client_auth();
-            
-        Ok(config)
-    }
-
-    async fn proxy_data<T, U>(&self, mut client: T, mut server: U) -> Result<(), anyhow::Error>
-    where
-        T: AsyncRead + AsyncWrite + Unpin,
-        U: AsyncRead + AsyncWrite + Unpin,
-    {
-        // Implementar forwarding bidirecional
-        // Usando tokio::io::copy_bidirectional ou similar
-        Ok(())
-    }
+pub async fn handle_ssh_tunnel(
+    mut client_stream: TcpStream,
+    target_addr: &str,
+) -> Result<(), Box<dyn Error>> {
+    info!("🔑 Estabelecendo túnel SSH para {}", target_addr);
+    
+    let mut server_stream = TcpStream::connect(target_addr).await?;
+    copy_bidirectional(&mut client_stream, &mut server_stream).await?;
+    
+    Ok(())
 }
