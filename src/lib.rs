4a7af716132eafd626d24579367c296509dfb564
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-21 17:54:26 -0300

    Remove 'pub' from module declarations

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-pub mod config;
-pub mod protocol;
-pub mod handler;
-pub mod security;
-pub mod websocket;
-pub mod tls;
-pub mod ssh;
-pub mod socks5;
-pub mod tcp_fallback;
+mod config;
+mod protocol;
+mod handler;
+mod security;
+mod websocket;
+mod tls;
+mod ssh;
+mod socks5;
+mod tcp_fallback;
 
 pub use config::Config;
 pub use handler::start_proxy;
