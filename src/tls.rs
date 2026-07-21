9924dab951a4f8d47be2a9a71f11c7b2b7bafdc0
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-21 17:46:55 -0300

    Update tls.rs

--- src/tls.rs
+++ src/tls.rs
@@ -1,12 +1,15 @@
 use tokio::net::TcpStream;
-use tokio_rustls::{TlsAcceptor, server::TlsStream};
+use tokio_rustls::TlsAcceptor;
 use rustls::{ServerConfig, Certificate, PrivateKey};
 use std::sync::Arc;
 use anyhow::Result;
-use rcgen::generate_simple_self_signed;
+use log::info;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-pub async fn get_tls_acceptor() -> Result<TlsAcceptor> {
-    let cert = generate_simple_self_signed(vec!["localhost".to_string()])?;
+pub async fn handle_tls(socket: TcpStream) -> Result<()> {
+    info!("🔒 TLS handshake...");
+    
+    let cert = rcgen::generate_simple_self_signed(vec!["localhost".to_string()])?;
     let cert_der = cert.serialize_der()?;
     let key_der = cert.serialize_private_key_der();
     
@@ -15,11 +18,23 @@ pub async fn get_tls_acceptor() -> Result<TlsAcceptor> {
         .with_no_client_auth()
         .with_single_cert(vec![Certificate(cert_der)], PrivateKey(key_der))?;
     
-    Ok(TlsAcceptor::from(Arc::new(config)))
-}
-
-pub async fn handle_tls_stream(socket: TcpStream) -> Result<TlsStream<TcpStream>> {
-    let acceptor = get_tls_acceptor().await?;
-    let tls_stream = acceptor.accept(socket).await?;
-    Ok(tls_stream)
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+    let mut tls_stream = acceptor.accept(socket).await?;
+    
+    info!("🔒 TLS handshake complete!");
+    
+    let mut buf = [0u8; 1024];
+    loop {
+        match tls_stream.read(&mut buf).await {
+            Ok(0) => break,
+            Ok(n) => {
+                let msg = String::from_utf8_lossy(&buf[..n]);
+                let response = format!("SECURE: {}", msg);
+                tls_stream.write_all(response.as_bytes()).await?;
+            }
+            Err(e) => anyhow::bail!("TLS error: {}", e),
+        }
+    }
+    
+    Ok(())
 }
